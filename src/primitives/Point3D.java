d9f020465501b0ab1395fc28e74eb767bce1bf5e
eliezerHorowitz <eli191191@gmail.com>
2020-03-22 04:52:20 +0200

    ghjghjghjg

---
+++ src/primitives/Point3D.java
@@ -0,0 +1,39 @@
+package primitives;
+
+public class Point3D {
+	
+	public class point3D {
+		 private int x;
+		 private int y;
+		 private int z;
+		 
+		 public void setX(int x) {
+			 this.x = x;
+		 }
+		 public int  getX() {
+			 return x;
+		 }
+		
+		 public void setY(int y) {
+			 this.y = y;
+		 }
+		 public int  getY() {
+			 return y;
+		 }
+		
+		 public void setZ(int z) {
+			 this.z = z;
+		 }
+		 public int  getZ() {
+			 return z;
+		 }
+		
+		public point3D(int x, int y, int z) {
+			setX(x);
+			setY(y);
+			setZ(z);
+		}
+		
+	}
+
+}
